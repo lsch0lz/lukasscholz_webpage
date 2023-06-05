749b39217b2aaee22574897db696758da1caf5ff
lsch0lz <lscholz14@googlemail.com>
2023-06-05 22:02:25 +0200

    feature: added basic styling for /home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 
 const Home: React.FC = () => {
     return (
         <div>
             <h1>Home</h1>
-            <p>Home page body content</p>
+            <img src={require("../static/profile_picture.jpg")} width={"300"} height={"400"}/>
+            <p>LinkedIn: <Link to={"https://www.linkedin.com/in/lukas-scholz-5b3a671a7/"}>Lukas Scholz</Link> </p>
+            <p>Github: <Link to={"https://github.com/lsch0lz"}>lsch0lz</Link></p>
+            <p>Twitter: <Link to={"https://twitter.com/lscholz_"}>lscholz_</Link></p>
         </div>
     );
 }
