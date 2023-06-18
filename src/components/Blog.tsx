620e777b2eff28def20bab07971ecd57dc3859fa
Lukas Scholz <lukas.scholz@deutschebahn.com>
2023-06-18 20:42:10 +0200

    added text for about & blog page

---
+++ src/components/Blog.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            <h1>Blog</h1>
+            <h2>Coming soon...</h2>
+        </div>
+    );
+}
+
+export default Blog;
