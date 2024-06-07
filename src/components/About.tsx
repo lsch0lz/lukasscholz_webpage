140a507297775eb52203a08abcf7b27d39cb50d9
Lukas Scholz <lukas.scholz@deutschebahn.com>
2024-06-07 10:18:38 +0200

    feat: updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,11 +4,11 @@ const About = () => {
     return (
         <div>
             <h1>About Me</h1>
-            <p>I'm 24 years old and work as an AI Engineer at <a href={"https://www.dbsystel.de/dbsystel/Consulting-und-Innovation/Ventures/vsion-ai-9108982"}>vsion.ai.</a> </p>
-            <p>I'm also currently doing a master's degree in computer science at Humboldt University in Berlin.</p>
+            <p>I'm 25 years old and work as an AI Engineer at <a href={"https://www.dbsystel.de/dbsystel/Consulting-und-Innovation/Ventures/vsion-ai-9108982"}>vsion.ai.</a> </p>
+            <p>I'm also currently doing a master's degree in computer science at Humboldt University in Berlin. (final year)</p>
             <p>My main interests are:</p>
             <li>Computer Vision</li>
-            <li>Sensorfusion&nbsp; &nbsp;</li>
+            <li>Explainable Artificial Intelligence&nbsp; &nbsp;</li>
             <li>Optimization of Neural Networks</li>
         </div>
     );
